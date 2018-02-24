59852c2d07b7612c11a34595653e3e185b939afc
PauBlanes_ENTI <paublanespardo@enti.cat>
2018-02-24 13:59:53 +0100

    Server amb socket selector i esperar a que tothom estigui conectat per començar

--- Taller2/Server/Server_Main.cpp
+++ Taller2/Server/Server_Main.cpp
@@ -3,13 +3,116 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <list>
 
 using namespace std;
 using namespace sf;
 
+void ServerManager()
+{
+	bool running = true;
+	// Create a socket to listen to new connections
+	TcpListener listener;
+	Socket::Status status = listener.listen(5000);
+	if (status != Socket::Done)
+	{
+		cout << "Error al abrir listener\n";
+		exit(0);
+	}
+	else
+		cout << "Port ok" << endl;
+
+	// Create a list to store the future clients
+	list<TcpSocket*> clients;
+	// Create a selector
+	SocketSelector selector;
+	// Add the listener to the selector
+	selector.add(listener);
+
+	//Define the number of players you want
+	int maxPlayers = 1;
+
+	// Endless loop that waits for new connections
+	while (running)
+	{
+		// Make the selector wait for data on any socket
+		if (selector.wait())
+		{
+			cout << clients.size() << endl;
+			// Test the listener
+			if (selector.isReady(listener))
+			{
+				// The listener is ready: there is a pending connection
+				TcpSocket* client = new TcpSocket;
+				if (listener.accept(*client) == Socket::Done)
+				{
+					// Add the new client to the clients list
+					std::cout << "Llega el cliente con puerto: " << client->getRemotePort() << std::endl;
+					clients.push_back(client);
+					// Add the new client to the selector so that we will
+					// be notified when he sends something
+					selector.add(*client);
+
+					//Si tots els jugadors ja estan conectats avisem
+					if (clients.size() == maxPlayers)
+						cout << "Todos los jugadores estan conectados!" << endl;
+					
+				}
+				else
+				{
+					// Error, we won't get a new connection, delete the socket
+					cout << "Error al recoger conexion nueva\n";
+					delete client;
+				}
+			}
+			else if (clients.size() == maxPlayers) //si estan tots els jugadors ja podem començar a rebre i enviar missatges
+			{
+				// The listener socket is not ready, test all other sockets (the clients)
+				list<TcpSocket*>::iterator it = clients.begin();
+				bool shouldErase;
+
+				while ( it != clients.end())
+				{
+					TcpSocket& client = **it;
+					if (selector.isReady(client))
+					{
+						// The client has sent some data, we can receive it
+						Packet packet;
+						status = client.receive(packet);
+						if (status == Socket::Done)
+						{
+							string strRec;
+							packet >> strRec;
+							cout << "He recibido " << strRec << " del puerto " << client.getRemotePort() << endl;
+						}
+						else if (status == Socket::Disconnected)
+						{
+							selector.remove(client);
+							shouldErase = true;
+							cout << "Elimino el socket que se ha desconectado" << endl;							
+						}
+						else
+						{
+							cout << "Error al recibir de " << client.getRemotePort() << endl;
+						}
+					}
+					//Si volem borrar elements de la llista hem de controlar que no ens sortim fora amb l'iterador
+					if (shouldErase)
+						it = clients.erase(it);
+					else
+						++it;
+				}
+			}
+		}
+	}
+
+}
+
 int main()
 {
-	cout << "Server" << endl;
+	ServerManager();
+	
+	/*cout << "Server" << endl;
 	//ESTABLIM CONEXIÓ
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();	
 	Socket::Status status;
@@ -36,7 +139,7 @@ int main()
 	}
 	cout << "All players are connected" << endl;
 	listener.close();
-	system("pause");
+	system("pause");*/
 	
 	
 	return 0;
